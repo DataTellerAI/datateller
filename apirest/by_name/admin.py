bf0960114f88ec873efe50c025435af81090216c
Campo Elias Pinillos <1207@holbertonschool.com>
2020-05-26 22:30:48 -0500

    [Delete] Files

--- apirest/by_name/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from by_name.models import Person
-
-admin.site.register(Person)
--- apirest/by_name/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from by_name.views import Consulta
-
-urlpatterns = [
-    path('<name>', Consulta.as_view()),
-]
