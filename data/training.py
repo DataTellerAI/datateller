33757f27654e684ed617bf5199420193966460c2
Campo Elias Pinillos <1207@holbertonschool.com>
2020-05-30 00:29:47 -0500

    [Update] Include training of the second ML Model

--- data/training.py
+++ data/training.py
@@ -1,5 +1,9 @@
 from model import GenderClassifier
+from model1 import GenderClassifier1
 
 clf = GenderClassifier()
 clf.ml_model(data_frame='/Users/campopinillos/Documents/Proyecto Final/nombresFull.xlsx')
-clf.load_model('saved_model.pickle')
+
+clf = GenderClassifier1()
+clf.ml_model(data_frame='/Users/campopinillos/Documents/Proyecto Final/nombresFull.xlsx',
+			 output='saved_model1.pickle')
