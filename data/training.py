ac2db168d07ada92c9bebf0675425fca0eaac4a0
Campo Elias Pinillos <1207@holbertonschool.com>
2020-06-01 10:33:20 -0500

    [Update] Third ml model

--- data/training.py
+++ data/training.py
@@ -1,9 +1,14 @@
 from model import GenderClassifier
 from model1 import GenderClassifier1
+from model2 import GenderClassifier2
 
-clf = GenderClassifier()
-clf.ml_model(df='/Users/campopinillos/Documents/Proyecto Final/nombresFull.xlsx')
+path = '/Users/campopinillos/Documents/Proyecto Final/nombresFull.xlsx'
+
+clf = GenderClassifier2()
+clf.ml_model(df=path, output='saved_model2.pickle')
 
 clf = GenderClassifier1()
-clf.ml_model(df='/Users/campopinillos/Documents/Proyecto Final/nombresFull.xlsx',
-			 output='saved_model1.pickle')
+clf.ml_model(df=path, output='saved_model1.pickle')
+
+clf = GenderClassifier()
+clf.ml_model(df=path)
